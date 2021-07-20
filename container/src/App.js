4b058d38212f04340154cf182a4dd89e1d0e362b
Diego Adilson Vieira <diegoavieira@gmail.com>
2021-07-20 20:40:44 -0300

    build: configuration

--- container/src/App.js
+++ container/src/App.js
@@ -1,13 +1,20 @@
 import React from 'react';
 import { BrowserRouter as Router } from 'react-router-dom';
+import { StylesProvider, createGenerateClassName } from '@material-ui/core/styles';
 import MarketingApp from './components/MarketingApp';
 import Header from './components/Header';
 
+const generateClassName = createGenerateClassName({
+  productionPrefix: 'co'
+});
+
 export default () => (
   <Router>
-    <div>
-      <Header />
-      <MarketingApp />
-    </div>
+    <StylesProvider generateClassName={generateClassName}>
+      <div>
+        <Header />
+        <MarketingApp />
+      </div>
+    </StylesProvider>
   </Router>
 );
