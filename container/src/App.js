3735ce274fe77209cedc3a12a7047a7b7426846a
Diego Adilson Vieira <diegoavieira@gmail.com>
2021-07-20 16:49:51 -0300

    build: configuration

--- container/src/App.js
+++ container/src/App.js
@@ -1,10 +1,13 @@
 import React from 'react';
+import { BrowserRouter as Router } from 'react-router-dom';
 import MarketingApp from './components/MarketingApp';
+import Header from './components/Header';
 
 export default () => (
-  <div>
-    <h1>Hi</h1>
-    <hr />
-    <MarketingApp />
-  </div>
+  <Router>
+    <div>
+      <Header />
+      <MarketingApp />
+    </div>
+  </Router>
 );
