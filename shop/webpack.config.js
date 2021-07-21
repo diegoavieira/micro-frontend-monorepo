ceb791e9d0f8402b9d575cf8c87ab056eedfdef0
Diego Adilson Vieira <diegoavieira@gmail.com>
2021-07-21 18:51:45 -0300

    build: configuration

---
+++ shop/webpack.config.js
@@ -0,0 +1,68 @@
+const ModuleFederationPlugin = require("webpack/lib/container/ModuleFederationPlugin");
+const mf = require("@angular-architects/module-federation/webpack");
+const path = require("path");
+const share = mf.share;
+
+const sharedMappings = new mf.SharedMappings();
+sharedMappings.register(path.join(__dirname, "tsconfig.json"), [
+  /* mapped paths to share */
+]);
+
+const port = 8084;
+
+module.exports = {
+  output: {
+    uniqueName: "shop",
+    publicPath: "auto",
+  },
+  optimization: {
+    runtimeChunk: false,
+  },
+  resolve: {
+    alias: {
+      ...sharedMappings.getAliases(),
+    },
+  },
+  plugins: [
+    new ModuleFederationPlugin({
+      // For remotes (please adjust)
+      name: "shop",
+      filename: "remoteEntry.js",
+      exposes: {
+        "./ShopApp": ".//src/app/app.module.ts",
+      },
+
+      // For hosts (please adjust)
+      // remotes: {
+      //     "mfe1": "mfe1@http://localhost:3000/remoteEntry.js",
+
+      // },
+
+      shared: share({
+        "@angular/core": {
+          singleton: true,
+          strictVersion: true,
+          requiredVersion: "auto",
+        },
+        "@angular/common": {
+          singleton: true,
+          strictVersion: true,
+          requiredVersion: "auto",
+        },
+        "@angular/common/http": {
+          singleton: true,
+          strictVersion: true,
+          requiredVersion: "auto",
+        },
+        "@angular/router": {
+          singleton: true,
+          strictVersion: true,
+          requiredVersion: "auto",
+        },
+
+        ...sharedMappings.getDescriptors(),
+      }),
+    }),
+    sharedMappings.getPlugin(),
+  ],
+};
