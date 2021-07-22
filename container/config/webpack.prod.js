fcb4a2b73fcc2aedcc8bab8c42c26bfa41814261
Diego Adilson Vieira <diegoavieira@gmail.com>
2021-07-22 15:11:32 -0300

    build: configuration

--- container/config/webpack.prod.js
+++ container/config/webpack.prod.js
@@ -17,7 +17,8 @@ const prodConfig = {
       remotes: {
         marketing: `marketing@${domain}/marketing/latest/remoteEntry.js`,
         auth: `auth@${domain}/auth/latest/remoteEntry.js`,
-        dashboard: `dashboard@${domain}/dashboard/latest/remoteEntry.js`
+        dashboard: `dashboard@${domain}/dashboard/latest/remoteEntry.js`,
+        shop: `shop@${domain}/shop/latest/remoteEntry.js`
       },
       shared: packageJson.dependencies
     })
