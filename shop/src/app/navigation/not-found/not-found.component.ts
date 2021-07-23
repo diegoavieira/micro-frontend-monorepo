c8e62ad925bad9b6d116bc98a98e2d321c96e124
Diego Adilson Vieira <diegoavieira@gmail.com>
2021-07-23 00:36:04 -0300

    build: configuration

--- shop/src/app/navigation/not-found/not-found.component.ts
+++ shop/src/app/navigation/not-found/not-found.component.ts
@@ -3,8 +3,7 @@ import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 @Component({
   selector: 'app-not-found',
   templateUrl: './not-found.component.html',
-  styleUrls: ['./not-found.component.scss'],
-  encapsulation: ViewEncapsulation.None
+  styleUrls: ['./not-found.component.scss']
 })
 export class NotFoundComponent implements OnInit {
   constructor() {}
